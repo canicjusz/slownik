7c3731706f999cfe72bc4f1cf734bacf72a23347
canicjusz <janek.bananek371@gmail.com>
2023-03-10 19:20:06 +0100

    storing db password in a .env file

--- connect-db.php
+++ connect-db.php
@@ -1,5 +1,6 @@
 <?php
-$mysqli = mysqli_connect("localhost", "root", "", "slownik");
+$env = parse_ini_file('.env');
+$mysqli = mysqli_connect("localhost", "root", $env['PASSWORD'] ?? '', "slownik");
 $mysqli->set_charset('utf8mb4');
 if ($mysqli->connect_error) {
   echo "Connection error:" . $mysqli->connect_error;
